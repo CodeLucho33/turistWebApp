30ea77ade4586fe3b0da53c3b3d3ddaa931d4960
CodeLucho33 <code.lucho33@gmail.com>
2024-09-09 06:53:06 -0500

    mobile first

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -8,17 +8,17 @@ export function Activity(props) {
                 <div className="activity-one">
                     <h2 className="title-top-activity">{props.actOne}</h2>
                     {props.imgOne? <img  className="img-top-activity"src={`${props.imgOne}`}/>:null}
-                    <p>{props.whyOne}</p>
+                    <p  className="txt-why">{props.whyOne}</p>
                 </div>
                 <div className="activity-two">
                     <h2 className="title-top-activity">{props.actTwo}</h2>
                   {props.imgTwo?<img  className="img-top-activity"src={`${props.imgTwo}`}/>:null}  
-                    <p>{props.whyTwo}</p>
+                    <p className="txt-why">{props.whyTwo}</p>
                 </div>
                 <div className="activity-three">
                     <h2 className="title-top-activity">{props.actThree}</h2>
                     {props.imgTh?<img  className="img-top-activity"src={`${props.imgTh}`} alt="Imagen "/>:null}
-                    <p>{props.whyTh}</p>
+                    <p className="txt-why">{props.whyTh}</p>
                 </div>
 
             </div>
