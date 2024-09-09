ff7a1bb8958f0f8b5636bd8070738bae0b7d7696
CodeLucho33 <code.lucho33@gmail.com>
2024-09-09 06:53:25 -0500

    mobile first

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,8 +14,8 @@ export function Card() {
             <div className="presentation-card">
 
                 <img className="my-photo" src={myPhoto} alt="" />
-                <div><h1>Luis Restrepo</h1>
-                    <p>Soy desarrollador frontend especialista en React</p></div>
+                <div><h1 className="my-name-card"><em><i>Luis Restrepo.</i></em></h1>
+                    <p>Soy entusiasta del desarrollo web. Disfruto crear proyectos que resuelvan problemas</p></div>
                 <button onClick={handleContact} className="btn-contact">Contact me!</button>
 
             </div>
